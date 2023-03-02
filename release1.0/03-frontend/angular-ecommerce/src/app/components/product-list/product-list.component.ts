001c1355f4595d3c42e849f8141356fc5e09c822
lilycuso4 <LilyCuSO4@163.com>
2023-03-02 16:28:19 -0500

    Updates of Angular Frontend

--- release1.0/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ release1.0/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
@@ -10,10 +10,18 @@ import { ProductService } from 'src/app/services/product.service';
   //templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
 
   products: Product[] = [];
   curentCategoryId: number = 1;
+  previousCategoryId: number = 1;
+  searchMode: boolean = false;
+
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
+  previousKeyword: string = "";
+
   constructor(private productService:ProductService,
               private route: ActivatedRoute) { }
 
@@ -23,6 +31,28 @@ export class ProductListComponent {
     });
   }
   listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword')!;
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    }
+    else {
+      this.handleListProducts();
+    }
+    
+  }
+
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+    if (this.previousKeyword != theKeyword) {
+      this.thePageNumber = 1;
+    }
+    this.previousKeyword = theKeyword;
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
+
+    this.productService.searchProductsPaginate(this.thePageNumber - 1, this.thePageSize,theKeyword).subscribe(this.processResult());
+  }
+
+  handleListProducts() {
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     if (hasCategoryId) {
       this.curentCategoryId = +this.route.snapshot.paramMap.get('id')!;
@@ -30,11 +60,31 @@ export class ProductListComponent {
     else {
       this.curentCategoryId = 1;
     }
-    this.productService.getProductList(this.curentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+
+    if(this.previousCategoryId != this.curentCategoryId) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.curentCategoryId;
+
+    console.log(`currentCategoryId=${this.curentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
+    this.productService.getProductListPaginate(this.thePageNumber - 1, 
+                                               this.thePageSize,
+                                               this.curentCategoryId).subscribe(this.processResult());
   }
 
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
+  processResult() {
+    return (data:any) => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    }
+  }
 }
